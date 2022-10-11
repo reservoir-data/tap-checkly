fbe2478c6e0485d7dadbe9ece3b73aeb08de924e
Edgar R. M <edgarrm358@gmail.com>
2022-10-11 13:38:44 -0500

    test: Add integration tests (#3)
    * test: Add integration tests
    
    * Make linter happy

--- tap_checkly/streams.py
+++ tap_checkly/streams.py
@@ -93,7 +93,7 @@ class AlertNotifications(ChecklyStream):
             th.StringType,
             description=(
                 "The result of sending the alert notification. For example, this could "
-                "be the response body of the Webhook.",
+                "be the response body of the Webhook."
             ),
         ),
         th.Property(
@@ -215,7 +215,7 @@ class Checks(ChecklyStream):
             th.BooleanType,
             description=(
                 "Determines if any notifications will be send out when a check fails "
-                "and/or recovers.",
+                "and/or recovers."
             ),
         ),
         th.Property(
@@ -477,7 +477,14 @@ class Checks(ChecklyStream):
                                 "method",
                                 th.StringType,
                                 default="POST",
-                                # enum=["GET", "POST", "PUT", "HEAD", "DELETE", "PATCH"],
+                                # enum=[
+                                #     "GET",
+                                #     "POST",
+                                #     "PUT",
+                                #     "HEAD",
+                                #     "DELETE",
+                                #     "PATCH",
+                                # ],
                             ),
                             th.Property("headers", th.ArrayType(KEY_VALUE_OBJECT)),
                             th.Property(
